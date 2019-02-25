aced9b178b9a0ea087fef97e27d23c13e7247b81
ChengXiang <2393239440@qq.com>
2019-02-25 23:35:31 +0800

    Ribbon负载均衡算法调试

--- microservicecloud-consumer-dept-80/src/main/java/com/union/cfgbean/ConfigBean.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/union/cfgbean/ConfigBean.java
@@ -1,5 +1,9 @@
 package com.union.cfgbean;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import com.netflix.loadbalancer.RetryRule;
+import com.netflix.loadbalancer.RoundRobinRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +19,17 @@ public class ConfigBean {
 
     @Bean
     @LoadBalanced   //客户端ribbon负载均衡
-    public RestTemplate getRestTemplate(){
+    public RestTemplate getRestTemplate() {
         return new RestTemplate();
+    }
+
+    /**
+     * ribbon负载均衡算法演示*/
+    @Bean
+    public IRule myRule() {
+//        return  new RoundRobinRule();   //默认的轮询算法
+//        return new RandomRule();    //随机算法
+        return new RetryRule(); //指定时间内过滤掉崩溃的服务
     }
 
 }
